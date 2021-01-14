cbf6c7e8b448f4b6b4b2723086c70989ba92b625
Justyna <justynka.takk@gmail.com>
2021-01-14 09:36:29 +0100

    addind card btn functionality

--- app.js
+++ app.js
@@ -51,6 +51,73 @@ console.log(template);
 const firstClone = template.content.firstElementChild.cloneNode(true);
 cardContainer.appendChild(firstClone);
 
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const collection = document.querySelector('.collection');
+
+form.addEventListener('submit', addCard);
+function addCard(e) { 
+    if(taskInput.value === '') {
+        alert('Add a task');
+    }    
+    const li =  document.createElement('li');  
+    li.className = 'card-item';
+    let inpt = document.createElement('input');
+    inpt.setAttribute("type", "checkbox");
+    li.appendChild(inpt);
+    let inpt2 = document.createElement('input');
+    inpt2.setAttribute("type", "text");
+    inpt2.setAttribute("value", taskInput.value);
+    li.appendChild(inpt2);    
+
+    let saveBtn = document.createElement('input');
+    saveBtn.setAttribute("type", "button");
+    saveBtn.setAttribute("value", "Save");
+    saveBtn.className = 'btn save hidden';
+    li.appendChild(saveBtn);
+
+    let editBtn = document.createElement('input');
+    editBtn.setAttribute("type", "button");
+    editBtn.setAttribute("value", "Edit");
+    editBtn.className = 'btn edit';
+    li.appendChild(editBtn);
+
+    let delBtn = document.createElement('input');
+    delBtn.setAttribute("type", "button");
+    delBtn.setAttribute("value", "X");
+    delBtn.className = 'btn delete';
+    li.appendChild(delBtn);
+    
+  
+    let divColors = document.createElement('div'); 
+    let bred = document.createElement('button'); 
+    let borange = document.createElement('button'); 
+    let byellow = document.createElement('button'); 
+    let bblue =document.createElement('button'); 
+    divColors.className = 'colors hidden';
+    divColors.appendChild(bred);
+    divColors.appendChild(borange);
+    divColors.appendChild(byellow);
+    divColors.appendChild(bblue);
+    bred.setAttribute("type", "button");
+    bred.className = 'btn-color red';
+    borange.setAttribute("type", "button");
+    borange.className = 'btn-color orange';
+    byellow.setAttribute("type", "button");
+    byellow.className = 'btn-color yellow';
+    bblue.setAttribute("type", "button");
+    bblue.className = 'btn-color blue';
+    li.appendChild(divColors);   
+
+    // console.log(li);
+    collection.appendChild(li);
+    taskInput.value = '';
+    e.preventDefault();  
+}
+
+
+
+
 
 // 
 //nastepne
