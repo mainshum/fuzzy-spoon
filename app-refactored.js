3d5a608b632dd58c78f915c95ac8d78dc0357614
Michał Czech <czechm11@gmail.com>
2021-01-19 13:48:07 +0100

    Default value of colors

--- app-refactored.js
+++ app-refactored.js
@@ -9,14 +9,9 @@ function addColor (String color, event) {
 }
 
 
-Function changeColor(List colors, event, String finalColor) {
+Function changeColor(event, String finalColor, List colors = ['redBg', 'blueBg', 'pinkBg', 'yellowBg']) {
 	colors.forEach(function (color) {
 		checkIfColorAndRemove(color, event)
 	}
 	addColor(finalColor, event)
-}
-
-const colors = ['redBg', 'blueBg', 'pinkBg', 'yellowBg']
-colors.forEach(function (finalColor) {
-	changeColor(colors, event, finalColor)
 }
