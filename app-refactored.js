1dff7c9da09d8bda60d5194508a86506b68acd85
leukocyt-byte <58117573+leukocyt-byte@users.noreply.github.com>
2021-01-20 13:40:42 +0100

    Update app-refactored.js
    selecting dom

--- app-refactored.js
+++ app-refactored.js
@@ -1,3 +1,25 @@
+const red = document.querySelectorAll(".red");
+const pink = document.querySelectorAll(".pink");
+const yellow = document.querySelectorAll(".yellow");
+const blue = document.querySelectorAll(".blue");
+
+
+console.log(red);
+
+function colorListeners() {
+    red.addEventListener('click', changeColor(redBg));
+    pink.addEventListener('click', changeColor(pinkBg));
+    yellow.addEventListener('click', changeColor(yellowBg));
+    blue.addEventListener('click', changeColor(blueBg));
+}
+
+function changeColor(color) {
+    e.target.parentElement.parentElement.parentElement.setAttribute("class", color);
+}
+
+colorListeners();
+
+
 function checkIfColorAndRemove (String color, event) {
 	if (event.target.parentElement.parentElement.parentElement.classList.contains(color)) {
 		event.target.parentElement.parentElement.parentElement.classList.remove(color);
