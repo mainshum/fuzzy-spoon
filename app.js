940dcc0a97e06aee17de76670d0bb63dff7332e1
pasatoai <aleksander@aims.ai>
2021-02-16 17:45:18 +0100

    Zmiany karteczkowe

--- app.js
+++ app.js
@@ -1,68 +1,44 @@
 const formColor = document.querySelector('form');
 const radios = formColor.elements.radioCol;
 
+const Pair = (x, y) => [x, y];
+
+const avColors =  [ 'redBg', 'yellowBg', 'blueBg', 'pinkBg', 'blank' ];
+const defaultColor = avColors[0];
+const cardTemplate = document.querySelector('#card_form');
+ 
 radios.forEach(radio => {
     radio.addEventListener('click', event => {
-      const classesToRemove = [ 'redBg', 'yellowBg', 'blueBg', 'pinkBg', 'blank' ];
-      classesToRemove.forEach(classToRemove => {
-        event.target.closest('.container').classList.remove(classToRemove);
-      })
-      event.target.closest('.container').classList.add(`${radios.value}`);
+        const cont = event.target.closest('.container');
+        cont.setAttribute('input-color', radios.value);
     });
   });
 
-const form = document.querySelector('form');
+const form = document.querySelector('#upper-card form');
 const taskInput = document.querySelector('.new-task-input');
 const collection = document.getElementById('collection');
     
-    form.addEventListener('submit', addCard);
-    function addCard(e) {
-        const setBg = document.getElementById('enter-card');
-        const checkedCol = Array.from(setBg.elements.radioCol).find(radio => radio.checked);
-        
-        let li = document.createElement('li');
-        li.classList.add('card-item'); 
-        li.innerHTML = `
-        <form name="card">
-        <div class="container ${checkedCol.value}">
-          <div class="flex-row">
-            <label class="checkbox-container">
-            <input type="checkbox">
-            <span class="checkmark"></span>
-            </label>                      
-            <input type="text" class="new-task-input" id="task" name="task" value="${taskInput.value}"> 
-              <button class="edit-item seconday-content btn-style">
-              <a><i class="fa fa-edit"></i></a></button>
-              <button class="remove-item seconday-content btn-style">
-              <a><i class="fa fa-remove"></i></a></button>
-            <button class="save-item seconday-content btn-style hidden">
-                <a><i class="fa fa-save"></i></a></button>
-            <input type="submit" value="Add" class="btn-style non-icon hidden">
-          </div>  
-          <div class="colors hidden">           
-          <label class="color-picker">
-          <input type="radio" name="radioCol" value="redBg">
-            <span class="red checkmark-color"></span>
-          </label>
-          <label class="color-picker">
-          <input type="radio" name="radioCol" value="pinkBg">
-              <span class="pink checkmark-color"></span>
-          </label>
-          <label class="color-picker">
-          <input type="radio" name="radioCol" value="yellowBg">
-            <span class="yellow checkmark-color"></span>
-          </label>
-          <label class="color-picker">
-          <input type="radio" name="radioCol" value="blueBg">
-            <span class="blue checkmark-color"></span>
-          </label>
-            </div>
-        </div>
-      </form>              
-        `
-        const mainCol = document.getElementById('main-color');
-        mainCol.classList.add('blank');
-        taskInput.value = '';
-        collection.appendChild(li);         
-        e.preventDefault();     
-    }
+form.addEventListener('submit', addCard);
+
+
+function addCard(e) {
+
+    const cont = e.target.querySelector('.container');
+    const colorSelected = cont.getAttribute('input-color');
+    const cardValue = cont.querySelector('#task').value;
+
+    const card = cardTemplate.cloneNode(true).content; // nowy element
+
+    card.querySelector('.container').setAttribute('input-color', colorSelected);
+    card.querySelector('.new-task-input').value = cardValue;
+    
+    let li = document.createElement('li');
+    li.classList.add('card-item'); 
+    li.appendChild(card);
+
+    const mainCol = document.getElementById('main-color');
+    mainCol.classList.add('blank');
+    taskInput.value = '';
+    collection.appendChild(li);         
+    e.preventDefault();     
+}
